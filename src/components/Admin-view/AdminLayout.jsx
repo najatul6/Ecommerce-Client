95ec97d950a47f317f1f52cd97973e6b5ca2e8ed
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 22:48:42 +0600

    add AdminHeader and AdminSidebar to AdminLayout component

--- src/components/Admin-view/AdminLayout.jsx
+++ src/components/Admin-view/AdminLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import AdminSidebar from "./AdminSidebar";
+import AdminHeader from "./AdminHeader";
 
 function AdminLayout() {
   return (
     <div className="flex min-h-screen w-full">
       {/* Admin Sidebar  */}
+      <AdminSidebar/>
       <div className="flex flex-1 flex-col">
         {/* Admin Header  */}
+        <AdminHeader/>
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6 ">
           <Outlet />
         </main>
