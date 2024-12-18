938f922073673ed10204a2fc652baf485d7e73bb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 22:12:37 +0600

    add AuthLayout component for user authentication interface

---
+++ src/components/userAuth/layout.jsx
@@ -0,0 +1,13 @@
+function AuthLayout(){
+    return (
+        <div className="flex min-h-screen w-full">
+            <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
+            <div className="max-w-md space-y-6 text-center text-primary-foreground">
+                <h1 className="text-4xl font-extrabold tracking-tight">Welcome to Ecommmerce shopping</h1>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default AuthLayout;
