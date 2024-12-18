2338f7fab3ee68c1fd03cce48a2d546219bf0128
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 23:57:32 +0600

    refactor CommonForm to accept dynamic form controls for improved flexibility

--- src/components/common/CommonForm.jsx
+++ src/components/common/CommonForm.jsx
@@ -1,7 +1,20 @@
 
-function CommonForm() {
+function CommonForm({formControls}) {
   return (
-    <div>CommonForm</div>
+    <form>
+        <div className="flex flex-col gap-3">
+        {
+            formControls?.map((controlItem, index) => {
+                return (
+                    <div key={index}>
+                        <label htmlFor={controlItem.name} className="text-sm font-semibold text-gray-600">{controlItem.label}</label>
+                        <input type={controlItem.type} id={controlItem.id} className="w-full px-4 py-2 mt-2 border rounded-lg" placeholder={controlItem.placeholder} />
+                    </div>
+                )
+            })
+        }
+        </div>
+    </form>
   )
 }
 
