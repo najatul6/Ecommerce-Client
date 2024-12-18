d85f032f713c3ce7d5c770e4bbc79d4017d1f189
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-19 01:47:37 +0600

    refactor CommonForm to support dynamic rendering of select and textarea components

--- src/components/common/CommonForm.jsx
+++ src/components/common/CommonForm.jsx
@@ -1,66 +1,79 @@
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 function CommonForm({ formControls }) {
-    function inputsByComponentType(getControlItem){
-        let element = null;
-        switch (getControlItem?.componentType) {
-            case 'input':
-                element=<Input
-                 name={getControlItem.name}
+  function inputsByComponentType(getControlItem) {
+    let element = null;
+    switch (getControlItem?.componentType) {
+      case "input":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+        break;
+      case "select":
+        element = (
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem?.options && getControlItem?.options?.length > 0
+                ? getControlItem?.options.map((optionItem) => (
+                    <SelectItem key={optionItem?.id} value={optionItem?.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+        break;
+      case "textarea":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+        break;
 
-                    placeholder={getControlItem.placeholder}
-                    id={getControlItem.name}
-                    type={getControlItem.type}
-                
-                />
-                break;
-            case 'select':
-                element=(
-                    <Select>
-                        
-                    </Select>
-                )
-                break;
-            case 'textarea':
-                element=<Input
-                 name={getControlItem.name}
-
-                    placeholder={getControlItem.placeholder}
-                    id={getControlItem.name}
-                    type={getControlItem.type}
-                
-                />
-                break;
-        
-            default:
-                element=<Input
-                 name={getControlItem.name}
-
-                    placeholder={getControlItem.placeholder}
-                    id={getControlItem.name}
-                    type={getControlItem.type}
-                
-                />
-                break;
-        }
-        return element;
+      default:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+        break;
     }
+    return element;
+  }
   return (
     <form>
       <div className="flex flex-col gap-3">
         {formControls?.map((controlItem, index) => {
           return (
             <div className="grid w-full gap-1.5" key={index}>
-              <Label
-                htmlFor={controlItem?.name}
-                className="mb-1"
-              >
+              <Label htmlFor={controlItem?.name} className="mb-1">
                 {controlItem.label}
               </Label>
-             {
-                inputsByComponentType[controlItem]()
-             }
+              {inputsByComponentType[controlItem]()}
             </div>
           );
         })}
