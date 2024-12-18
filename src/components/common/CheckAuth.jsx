c811f9bedefa35ccdb580ce436d90f5e7d903dcf
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 23:23:12 +0600

    enhance CheckAuth component to handle authentication redirects based on user role

--- src/components/common/CheckAuth.jsx
+++ src/components/common/CheckAuth.jsx
@@ -1,8 +1,21 @@
+import { Navigate, useLocation } from "react-router-dom";
 
-function CheckAuth() {
-  return (
-    <div>CheckAuth</div>
-  )
+function CheckAuth({ isAuthenticated, user, children }) {
+  const location = useLocation();
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/signIn") ||
+      location.pathname.includes("/signUp")
+    )
+  ) {
+    return <Navigate to="/userAuth/signIn" />;
+  }
+    if (isAuthenticated && location.pathname.includes("/signIn")|| location.pathname.includes("/signUp")) {
+        if(user?.role === "admin") return <Navigate to="/admin/dashboard" />
+        return <Navigate to="/user/home" />;
+    }
+  return <div>CheckAuth</div>;
 }
 
-export default CheckAuth
+export default CheckAuth;
