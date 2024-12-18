415f81aa62756cba07b8bb1f939f8e951b207f0e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 23:28:12 +0600

    enhance CheckAuth component to improve authentication redirection logic based on user role and current path

--- src/components/common/CheckAuth.jsx
+++ src/components/common/CheckAuth.jsx
@@ -2,6 +2,8 @@ import { Navigate, useLocation } from "react-router-dom";
 
 function CheckAuth({ isAuthenticated, user, children }) {
   const location = useLocation();
+
+//   Check if the user is authenticated and redirect to the appropriate page
   if (
     !isAuthenticated &&
     !(
@@ -11,11 +13,29 @@ function CheckAuth({ isAuthenticated, user, children }) {
   ) {
     return <Navigate to="/userAuth/signIn" />;
   }
-    if (isAuthenticated && location.pathname.includes("/signIn")|| location.pathname.includes("/signUp")) {
-        if(user?.role === "admin") return <Navigate to="/admin/dashboard" />
-        return <Navigate to="/user/home" />;
-    }
-  return <div>CheckAuth</div>;
+
+//   Check if the user is authenticated and redirect to the appropriate page
+  if (
+    (isAuthenticated && location.pathname.includes("/signIn")) ||
+    location.pathname.includes("/signUp")
+  ) {
+    if (user?.role === "admin") return <Navigate to="/admin/dashboard" />;
+    return <Navigate to="/user/home" />;
+  }
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("user")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
 }
 
 export default CheckAuth;
