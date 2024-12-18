ddf6bb1a9543695131c88e6c869d7d3c510bcae9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 23:53:51 +0600

    add email, password, confirm password fields and submit button to signUpFormControls

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,32 @@ export const signUpFormControls=[
         placeholder:"Enter your user name",
         componentType:"input",
         type:"text",
+    },
+    {
+        name:"email",
+        label:"Email",
+        placeholder:"Enter your email",
+        componentType:"input",
+        type:"email",
+    },
+    {
+        name:"password",
+        label:"Password",
+        placeholder:"Enter your password",
+        componentType:"input",
+        type:"password",
+    },
+    {
+        name:"confirmPassword",
+        label:"Confirm Password",
+        placeholder:"Confirm your password",
+        componentType:"input",
+        type:"password",
+    },
+    {
+        name:"submit",
+        label:"Sign Up",
+        componentType:"button",
+        type:"submit",
     }
 ]
