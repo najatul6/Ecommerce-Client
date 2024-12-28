3e50396faebeda75b08e0a7469b325854d1fffd8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-28 22:39:16 +0600

    rename Login and Register components to AuthLogin and AuthRegister in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
-import Register from "./pages/auth/Register";
-import Login from "./pages/auth/Login";
+import AuthRegister from "./pages/auth/Register";
+import AuthLogin from "./pages/auth/Login";
 
 function App() {
   
@@ -9,8 +9,8 @@ function App() {
     <div className="flex flex-col overflow-hidden bg-white">
      <Routes>
       <Route path="/auth" element={<AuthLayout />}>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<AuthRegister />} />
+        <Route path="/login" element={<AuthLogin />}/>
       </Route>
      </Routes>
     </div>
