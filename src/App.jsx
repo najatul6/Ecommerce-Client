ac74beb99a2d7259f7492a0f0ccec0cb558cd033
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-28 22:56:27 +0600

    feat: update admin routing to include dashboard, products, orders, and features components

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import AuthLayout from "./components/auth/layout";
 import AuthRegister from "./pages/auth/Register";
 import AuthLogin from "./pages/auth/Login";
 import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
 
 function App() {
-  
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-     <Routes>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="register" element={<AuthRegister />} />
+          <Route path="login" element={<AuthLogin />} />
+        </Route>
 
-
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="register" element={<AuthRegister />} />
-        <Route path="login" element={<AuthLogin />}/>
-      </Route>
-
-      <Route path="/admin" element={<AdminLayout/>}>
-        <Route path="dashboard" element={<div>Dashboard</div>}/>
-        <Route path="products" element={<div>Products</div>}/>
-        <Route path="orders" element={<div>Orders</div>}/>
-        <Route path="customers" element={<div>Customers</div>}/>
-
-      </Route>
-     </Routes>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
