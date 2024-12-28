169ed10ef8fd8300e1655ca62479161b3aec573f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-28 22:06:09 +0600

    refactor main.jsx for improved formatting and consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
