3629e3b5303f2ac44d6d625dcfae64db781d2e35
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-30 00:00:58 +0600

    fix: reorder imports in main.jsx for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import store from "./store/store";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
