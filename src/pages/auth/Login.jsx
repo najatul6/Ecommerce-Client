264ac0c5eebfa242b7abc581147a5fcb4396c834
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-30 01:15:50 +0600

    feat: implement AuthLogin component with registration form and navigation

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,32 @@
+import CommonForm from "@/components/common/CommonForm"
+import { registerFormControls } from "@/config"
 
 function AuthLogin() {
   return (
-    <div>AuthLogin</div>
+    <div className="w-full mx-auto max-w-md space-y-6">
+    <div className="text-center">
+      <h1 className="text-3xl font-bold tracking-tight text-foreground">
+        Create Account
+      </h1>
+    </div>
+    <CommonForm
+      formControls={registerFormControls}
+      buttonText="Sign Up"
+      formData={formData}
+      setFormData={setFormData}
+      onSubmit={onSubmit}
+    />
+    <p className="mt-2 text-center">
+      Already have an account?
+      <Link
+        to="/auth/login"
+        className="font-medium text-primary hover:underline ml-2"
+      >
+        {" "}
+        Log In
+      </Link>
+    </p>
+  </div>
   )
 }
 
