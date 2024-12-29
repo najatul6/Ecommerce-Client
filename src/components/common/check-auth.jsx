da8e173b8e1e87c89d261b5876d94d87007fd619
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-29 23:37:05 +0600

    refactor: clean up CheckAuth component by removing unused PropTypes validation

--- src/components/common/check-auth.jsx
+++ src/components/common/check-auth.jsx
@@ -1,25 +1,14 @@
 import { Navigate, useLocation } from "react-router-dom";
 
-import PropTypes from 'prop-types';
-
 function CheckAuth({ isAuthenticated, user, children }) {
-    const location = useLocation();
-    
-    if (
-        !isAuthenticated &&
-        !(location.pathname === "/login" || location.pathname === "/register")
-    ) {
-        return <Navigate to="/auth/login" />;
-    }
-
-    return children;
+  const location = useLocation();
+  
+  if (
+    !isAuthenticated &&
+    !(location.pathname === "/login" || location.pathname === "/register")
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
 }
-
-// PropTypes validation
-CheckAuth.propTypes = {
-    isAuthenticated: PropTypes.bool.isRequired,
-    user: PropTypes.object,
-    children: PropTypes.node.isRequired,
-};
 
 export default CheckAuth;
