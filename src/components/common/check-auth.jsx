83ba5ba5958d2fe3f68f5702776ab2d5a92edd95
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-29 23:36:57 +0600

    feat: add PropTypes validation to CheckAuth component

--- src/components/common/check-auth.jsx
+++ src/components/common/check-auth.jsx
@@ -1,14 +1,25 @@
 import { Navigate, useLocation } from "react-router-dom";
 
+import PropTypes from 'prop-types';
+
 function CheckAuth({ isAuthenticated, user, children }) {
-  const location = useLocation();
-  
-  if (
-    !isAuthenticated &&
-    !(location.pathname === "/login" || location.pathname === "/register")
-  ) {
-    return <Navigate to="/auth/login" />;
-  }
+    const location = useLocation();
+    
+    if (
+        !isAuthenticated &&
+        !(location.pathname === "/login" || location.pathname === "/register")
+    ) {
+        return <Navigate to="/auth/login" />;
+    }
+
+    return children;
 }
+
+// PropTypes validation
+CheckAuth.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    user: PropTypes.object,
+    children: PropTypes.node.isRequired,
+};
 
 export default CheckAuth;
