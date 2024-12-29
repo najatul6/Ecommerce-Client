316780fda6e291a6b00d26359a9a66f4dac51227
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-29 23:40:19 +0600

    feat: add admin role redirection in CheckAuth component

--- src/components/common/check-auth.jsx
+++ src/components/common/check-auth.jsx
@@ -11,7 +11,9 @@ function CheckAuth({ isAuthenticated, user, children }) {
   }
 
   if(isAuthenticated && (location.pathname === "/login" || location.pathname==="/register")){
-    
+    if(user.role === "admin"){
+        return <Navigate to="/admin/dashboard" />
+    }
   }
 }
 
