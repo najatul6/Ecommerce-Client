754633b099ce25c99680be1c29696ead40cb6c52
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-30 01:13:15 +0600

    refactor: clean up formatting and improve readability in Register component

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,14 +1,13 @@
-
 import CommonForm from "@/components/common/CommonForm";
 import { registerFormControls } from "@/config";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const initialState = {
-  userName:'',
-  email: '',
-  password: '',
-}
+  userName: "",
+  email: "",
+  password: "",
+};
 
 function AuthRegister() {
   const [formData, setFormData] = useState(initialState);
@@ -16,18 +15,26 @@ function AuthRegister() {
   return (
     <div className="w-full mx-auto max-w-md space-y-6">
       <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">Create Account</h1>
-        <p className="mt-2">
-          Already have an account? 
-          <Link to="/auth/login" className="font-medium text-primary hover:underline ml-2"> Log In</Link>
-        </p>
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create Account
+        </h1>
       </div>
-      <CommonForm 
-      formControls={registerFormControls}
-      buttonText="Sign Up"
-      formData={formData}
-      setFormData={setFormData}
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText="Sign Up"
+        formData={formData}
+        setFormData={setFormData}
       />
+      <p className="mt-2 text-center">
+        Already have an account?
+        <Link
+          to="/auth/login"
+          className="font-medium text-primary hover:underline ml-2"
+        >
+          {" "}
+          Log In
+        </Link>
+      </p>
     </div>
   );
 }
