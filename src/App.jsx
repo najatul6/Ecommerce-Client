0ee2e3735a8dd04a5988bea7e074960359d1ec25
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-29 23:54:08 +0600

    fix: format JSX elements for better readability in App component

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,28 @@ function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={
+          
+          <AuthLayout />
+      }>
           <Route path="register" element={<AuthRegister />} />
           <Route path="login" element={<AuthLogin />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin" element={
+          
+          <AdminLayout />
+      }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
 
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route path="/shop" element={
+          
+          <ShoppingLayout />
+      }>
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
