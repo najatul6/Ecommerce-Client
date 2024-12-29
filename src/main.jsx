069cb9b663c3018884ef6de69d44b0d93a0a4794
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-30 00:08:28 +0600

    fix: remove StrictMode wrapper from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
-  </StrictMode>
 );
