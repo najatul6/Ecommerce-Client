61e0a52c4d687c5752b4253bc3506fa3ad1b7efc
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-29 23:39:02 +0600

    feat: add conditional navigation for authenticated users in CheckAuth component

--- src/components/common/check-auth.jsx
+++ src/components/common/check-auth.jsx
@@ -2,12 +2,16 @@ import { Navigate, useLocation } from "react-router-dom";
 
 function CheckAuth({ isAuthenticated, user, children }) {
   const location = useLocation();
-  
+
   if (
     !isAuthenticated &&
     !(location.pathname === "/login" || location.pathname === "/register")
   ) {
     return <Navigate to="/auth/login" />;
+  }
+
+  if(isAuthenticated && (location.pathname === "/login" || location.pathname==="/register")){
+    
   }
 }
 
