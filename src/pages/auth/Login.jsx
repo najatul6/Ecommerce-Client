878df6c7a0e67b5a8653213d0f04904b33510a0c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-30 01:17:09 +0600

    feat: update AuthLogin component for login functionality and form handling

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,17 +1,30 @@
 import CommonForm from "@/components/common/CommonForm"
 import { registerFormControls } from "@/config"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 function AuthLogin() {
+  const [formData, setFormData] = useState(initialState);
+  
+    function onSubmit() {
+  
+    }
   return (
     <div className="w-full mx-auto max-w-md space-y-6">
     <div className="text-center">
       <h1 className="text-3xl font-bold tracking-tight text-foreground">
-        Create Account
+       Log In
       </h1>
     </div>
     <CommonForm
       formControls={registerFormControls}
-      buttonText="Sign Up"
+      buttonText="Sign In"
       formData={formData}
       setFormData={setFormData}
       onSubmit={onSubmit}
@@ -19,11 +32,10 @@ function AuthLogin() {
     <p className="mt-2 text-center">
       Already have an account?
       <Link
-        to="/auth/login"
+        to="/auth/register"
         className="font-medium text-primary hover:underline ml-2"
       >
-        {" "}
-        Log In
+       Create Account
       </Link>
     </p>
   </div>
