30a7a00ce341d38dd513fcf25baa6ff07b4bc915
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-05 22:23:14 +0600

    feat: implement user registration with async thunk in auth slice

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -1,24 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-    isAuthenticated: false,
-    isLoading: false,
-    user: null,
-}
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+};
 
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (formData) => {
+    const res = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      formData,
+      { withCredentials: true }
+    );
 
-
+    return res.data;
+  }
+);
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers:{
-        setUser:(state,action)=>{
-
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+  },
+  extraReducers: builder=>{
+    builder.addCase(registerUser.pending,state=>{
+        state.isLoading=true;
+    }).addCase(registerUser.fulfilled,(state,action)=>{
+        state.isLoading=false;
+        state.user=action.payload;
+    })
+  }
+});
 
 export const { setUser } = authSlice.actions;
 export default authSlice.reducer;
