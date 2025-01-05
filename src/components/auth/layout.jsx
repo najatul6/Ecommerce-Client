89e2dc5b856fa81ed6c0476fed3b0a918f4b160a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-05 22:09:39 +0600

    feat: update Lottie integration in AuthLayout for improved animation handling

--- src/components/auth/layout.jsx
+++ src/components/auth/layout.jsx
@@ -1,16 +1,9 @@
-import Lottie from "react-lottie";
 import { Outlet } from "react-router-dom";
 import animationData from "../../assets/developer.json"
+import Lottie from "lottie-react";
 
 function AuthLayout() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  
   return (
     <div className="flex min-h-screen w-full">
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
@@ -18,7 +11,7 @@ function AuthLayout() {
           <h1 className="text-4xl font-extrabold tracking-tight capitalize">
             Welcome to E-Commerce shopping
           </h1>
-          <div className="w-full"><Lottie options={defaultOptions}/></div>
+          <div className="w-full"><Lottie animationData={animationData}/></div>
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
