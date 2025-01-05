7a1294adfa9d147fdb05b33848a312b1f32fad81
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-06 01:11:22 +0600

    feat: display success toast notification on successful user registration

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,6 @@
 import CommonForm from "@/components/common/CommonForm";
 import { registerFormControls } from "@/config";
+import { useToast } from "@/hooks/use-toast";
 import { registerUser } from "@/store/auth-slice";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
@@ -15,10 +16,15 @@ function AuthRegister() {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {toast}=useToast()
   function onSubmit(e) {
     e.preventDefault();
     dispatch(registerUser(formData)).then((data) => {
       if (data.payload.success) {
+        toast({
+          title: data.payload.message,
+          type: "success",
+        });
         navigate("/auth/login");
       }
     });
